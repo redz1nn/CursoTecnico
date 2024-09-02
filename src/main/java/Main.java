f0705611843cfde19bc3a9f79d0f3bfed7212b84
Victor Fabris <victor.fabris.santos@escola.pr.gov.br>
2024-09-02 12:17:26 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(" Curso Técnico:");
+    System.out.println("   Desenvolvimento de Sistemas");
+    System.out.println("");
+    System.out.println(" Aqui farei um breve resumo da minha opinião até agora sobre o curso técnico de Desenvolvimento de Sistemas.");
+    System.out.println("  Em Lógica Computacional tivemos como objetivo aprender o básico sobre preposições e linguagem de programação.");
+    System.out.println("  Em outras Matérias do curso como Ciência da Computação e Análise e Projeto de Sistemas, tivemos como objetivo aprender um pouco sobre o conceito de computação, como por exemplo oque é computação, daonde surgiu esse termo, e no qual aprendemos oque é hardware e Software, e também aprendemos que Hardware é a parte do computador que conseguimos tocar fisícamente e o Software é aquilo que não conseguimos tocar no sentido fisíco, como servidores, um exemplo pode ser o youtube que é um Software e nao um Hardware pois não conseguimos tocar fisicamente.");
+    System.out.println("   Então resumidamente, Lógica Computacional foi mais sobre línguagem dos computadores e programação, em Ciência Computacional e Análise e Projeto de Sistemas foi em geral do básico da área dos computadores.");
+  }
+}
